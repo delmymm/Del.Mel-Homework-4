f0983f32eeac158d2e515752e788446252a83b56
Delmy Melendez <delmymm93@gmail.com>
2020-10-08 16:53:20 -0600

    Added html,js, and css plus some code added to the html and CSS file.

---
+++ script.js
@@ -0,0 +1,13 @@
+var startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+
+function startGame(){
+
+}
+function nextQuestion() {
+    
+}
+function selectAnswer() {
+    
+}
