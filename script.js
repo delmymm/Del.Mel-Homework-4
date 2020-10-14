da1b5303f5659a83f82b298a4428bb1ea54a63ae
Delmy Melendez <delmymm93@gmail.com>
2020-10-13 21:12:56 -0600

    Additional JS and HTML code added plus deletions on JS.

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ var answerBtn3 = document.getElementById('btn3')
 var answerBtn4 = document.getElementById('btn4')
 var highscore = document.getElementById('highscore')
 var answerButtons = document.getElementById('answer-buttons')
+var messageAreaEl = document.getElementById('message-area');
+var nameSubmitEl = document.getElementById('name-submit');
 var scoreBoolean = false;
+var scoreArray = [];
+var currentScore = 0;
 var time = 60;
 var questionNumber = 0;
 var availableQuestions = [];
@@ -50,6 +54,32 @@ startButton.addEventListener('click', function (event) {
     getNewQuestion();
 
 })
+var answerEls = document.querySelectorAll('#answer-buttons button');
+for (i = 0; i < answerEls.length; i++) {
+    answerEls[i].addEventListener('click', function (event) {
+        var answerNum = event.target.getAttribute('data-answer');
+        console.log(answerNum);
+        answerNum = answerNum - 1; // harmonize with questions array
+        var currentQuestion = questionsAnswers[questionNumber];
+        if (currentQuestion.answer == answerNum) {
+            //alert('Yay!!!');
+            messageAreaEl.innerHTML = '';
+            if (questionNumber < (totalQuestions - 1)) {
+                availableQuestions.push(questionsAnswers[questionNumber]);
+                questionNumber++;
+                getNewQuestion();
+                currentScore = currentScore + 10;
+                timerEl.innerHTML = currentScore;
+            } else {
+                endGame();
+            }
+        } else {
+            //alert('Bxzzz!!');
+            time = time - 10;
+            messageAreaEl.innerHTML = 'Wrong !!!!';
+        }
+    })
+}
 function timer() {
     var timer = setInterval(function () {
         if (time === 0) {
@@ -65,13 +95,13 @@ var totalQuestions = questionsAnswers.length;
 console.log(questionNumber < totalQuestions - 1);
 console.log(questionNumber);
 console.log(totalQuestions);
-nextButton.addEventListener('click', function (event) {
-    if (questionNumber < totalQuestions - 1) {
+/* nextButton.addEventListener('click', function (event) {
+    if (questionNumber < (totalQuestions - 1)) {
         availableQuestions.push(questionsAnswers[questionNumber]);
         questionNumber++;
         getNewQuestion();
     }
-})
+})*/
 
 function getNewQuestion() {
     questions.textContent = questionsAnswers[questionNumber].question;
@@ -79,4 +109,36 @@ function getNewQuestion() {
     answerBtn2.textContent = questionsAnswers[questionNumber].choices[1];
     answerBtn3.textContent = questionsAnswers[questionNumber].choices[2];
     answerBtn4.textContent = questionsAnswers[questionNumber].choices[3];
-} 
+}
+
+/*
+    called when name of winner is submitted
+*/
+function winnerName() {
+    let initials = document.getElementById('name').value;
+    let winner = {
+        initials: initials,
+        score: currentScore
+    }
+    scoreArray.push(winner);
+    showHighScores();
+
+}
+
+function showHighScores() {
+    let scoreContent = '';
+    for (i = 0; i < scoreArray.length; i++) {
+        scoreContent += scoreArray[i].initials + ': ' + scoreArray[i].score + '<br>';
+    }
+    let scoreEl = document.querySelector('#highScores .modal-body');
+    scoreEl.innerHTML = scoreContent;
+    $("#highScores").modal();
+}
+
+document.getElementById('highScoreLink').addEventListener('click', showHighScores());
+
+function endGame() {
+    alert('Game ended!!!');
+    userName.style.display = 'block';
+    nameSubmitEl.addEventListener('click', winnerName);
+}
